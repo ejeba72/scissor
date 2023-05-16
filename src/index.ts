9f7301b9f204db7b9322a7dfca13f5cfd99c6179
Emmanuel Eni <eejeba@gmail.com>
2023-05-16 11:36:58 +0100

    included explicit types to the various variables and function definitions in the index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
 import { config } from 'dotenv';
 
 config();
 
-const app = express();
-const PORT = process.env.PORT;
+const app: Application = express();
+const PORT: string | undefined = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/', ( req: Request, res: Response ): void => {
     console.log(`testing setup`);
     res.send(`hello, world`);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, (): void => {
     console.log(`App is attentively listening for incoming requests @ http://127.0.0.1:${PORT}`);
 });
