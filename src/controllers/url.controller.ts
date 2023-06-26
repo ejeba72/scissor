7b205ab20f842cc2a9b3b96e9b09f7cbbcbc04d6
Emmanuel Eni <eejeba@gmail.com>
2023-06-26 20:49:04 +0100

    work in progress: implement functionality that would delete qrcode images from the /public/img/ directory when such images have been loaded at the client side

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -8,9 +8,16 @@ import { qrGenerator, qrcodeResMsg } from '../utils/qrcode.util';
 import { join } from 'path';
 import { GetPublicKeyOrSecret, Secret, verify } from 'jsonwebtoken';
 import { generateUserId } from '../utils/userId.utils';
+import { unlink } from 'fs';
+import { URL } from 'url';
 
 config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as Secret | GetPublicKeyOrSecret;
+
+
+// function responseAndDelete(res: Response, qrcodeImagePath: any) {
+
+// }
 
 // @route POST /api/v1
 // @desc create short url  Response<any, Record<string, any>>
@@ -70,6 +77,9 @@ async function postNewShortUrl(req: Request, res: Response): Promise<unknown> {
                 shortUrlCreated: true,
                 resMsg: `Short url created! Short Url: "${shortUrl}", ${qrcodeResMsg(qrcodeRequested)}, Long url: "${longUrl}."`
             });
+            // if (qrcodeRequested) {
+            //     await deleteQrcodeImage(qrcodeFilePath);
+            // }
         } else {
             res.status(404).json({errMsg: `Please enter a valid long url.`});
         }
@@ -85,10 +95,44 @@ async function getDashboard(req: Request, res: Response) {
     try {
         // const decodedToken = verify(req.cookies?.jwt, JWT_SECRET_KEY);
         // const userId = (decodedToken as any)?.id;
-        console.log({ jwtToken: req.cookies?.jwt, JWT_SECRET_KEY });
+        // console.log({ jwtToken: req.cookies?.jwt, JWT_SECRET_KEY });
         const userId = generateUserId(req.cookies?.jwt, JWT_SECRET_KEY);
         console.log({userId});
         const urlCollection = await UrlModel.find({ userId });
+
+
+        // const qrcodeRequested = '';  // actually a boolean. 1. make a collection of urls documents that require qrcode images, using the filter method.
+        // // 2. get the file paths and shorturl of the documents that require qrcode images.
+        // // const qrcodeFilePath = join(__dirname, '..', '..', 'public', 'img', qrcodeFileName);
+
+        const qrcodeDocs = urlCollection.filter(doc => {
+            return doc.qrcodeRequested === true;
+        });
+        // console.log({qrcodeDocs});
+        const generatorParams = qrcodeDocs.map(doc => {
+            return {
+                qrcodeFilePath: join(__dirname, '..', '..', 'public', doc.qrcodeFileLocation),
+                shortUrl: doc.shortUrl,
+            }
+        });
+        // console.log({generatorParams});
+        // generatorParams.forEach(params => {
+        //     qrGenerator(params.qrcodeFilePath, params.shortUrl);
+        // });
+
+        generatorParams.forEach(params => {
+            async function generatorFunction() {
+                await qrGenerator(params.qrcodeFilePath, params.shortUrl);
+            }
+            generatorFunction();
+        });
+
+
+        // if (qrcodeRequested) {
+        //     await qrGenerator(qrcodeFilePath, shortUrl);
+        // }
+
+
         res.status(200).render('dashboard', { urlCollection });
     } catch (err: unknown) {
         res.status(500).render('500-page');
@@ -96,6 +140,27 @@ async function getDashboard(req: Request, res: Response) {
         console.log(err); 
     }
 }
+async function deleteQrcodeImage(req: Request, res: Response) {
+    try {
+        console.log({ reqBody: req.body });
+        const url = new URL(req.body.qrcodeImageToDelete);
+        const qrcodeImagePath = url.pathname;
+        console.log({ qrcodeImagePath });
+        const qrcodeFilePath = join(__dirname, '..', '..', 'public', qrcodeImagePath);
+        console.log({qrcodeFilePath});
+        // const imagePath = '';
+        await unlink(qrcodeFilePath, (err) => {
+            if (err) throw err;
+            console.log(`QRCode image was deleted`);
+        });
+        res.status(200).send();
+    } catch (err: unknown) {
+        res.status(500).send();
+        if (err instanceof Error) return console.log(err.message);
+        return console.log(err);
+    }
+}
+
 // update, deleteOne, deleteAll
 async function getUrl() {}
 async function getAllUrls() {}
@@ -103,4 +168,4 @@ async function updateUrl() {}
 async function deleteUrl() {}
 async function deleteAllUrls() {}
 
-export { postNewShortUrl, getDashboard, };
+export { postNewShortUrl, getDashboard, deleteQrcodeImage };
--- src/routes/url.route.ts
+++ src/routes/url.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getDashboard, postNewShortUrl, } from "../controllers/url.controller";
+import { deleteQrcodeImage, getDashboard, postNewShortUrl, } from "../controllers/url.controller";
 import { verifyJwtToken } from "../middleware/auth.middleware";
 
 const router = Router();
 // router.use('/', verifyJwtToken);
 router.route('/').post(postNewShortUrl).get(getDashboard);
+router.route('/delete').post(deleteQrcodeImage);
 
 export { router as urlRoute };
